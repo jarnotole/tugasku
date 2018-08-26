9cacea2fc4389f71558c044a878a0ec59d94a61b
jarnotole <40926989+jarnotole@users.noreply.github.com>
2018-08-26 03:33:53 -0500

    Add files via upload

---
+++ profile.php
@@ -0,0 +1,39 @@
+<head>
+	<meta charset="utf-8">
+	<!-- pemanggilan file css -->
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta name="viewport" content="width=device-widget, initial-scale=1">
+		<title>kulgram</title>
+</head>
+	<?php
+ require('header.php');
+ ?>
+		<h1>My Profile</h1>
+	<div class="pro" >
+			<img src="image/satu.jpg" width=" 150px">
+			<ul type="circle">
+				<li>Nama	: Sujarno</li>
+				<li>Nim		: 311610471</li>
+				<li>Kelas	: TI.16.B2</li>
+			</ul>
+	</div>
+	<div class="data">
+		<ul type="circle">
+			<h2>Biodata Lengkap</h2>
+			<li>Nama			:Sujarno</li>
+			<li>Jenis Kelamin	:Laki - Laki</li>
+			<li>Ttl 			:Purworejo, 22 Maret 1993/li>
+			<li>Alamat			:Cikarang</li>
+			<li>Agama 			:Islam</li>
+			<li>Kewarganegaraan :Indonesia</li>
+			<li>No.hp 			:087823813853</li>
+			<li>E-mail 			:sujarno@mhs.pelitabangsa.ac.id</li>
+		</ul>
+	</div>
+ <?php
+include_once 'footer.php';
+ ?>
+
+
+</body>
+</html>
